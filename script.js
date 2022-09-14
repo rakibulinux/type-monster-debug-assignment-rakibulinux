c405fb9aba55def9fc29de753199b76d9d20fbdc
Rakib Linux <rakibulinux@gmail.com>
2022-09-14 23:54:19 +0600

    Make responsive

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = (finishTime - startTime).toFixed(0) / 1000;
 
   // show result modal
   resultModal.innerHTML = "";
@@ -80,12 +80,12 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTaken.toFixed(0)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
 
-  addHistory(questionText, timeTaken, errorCount);
+  addHistory(questionText, timeTaken.toFixed(0), errorCount);
 
   // restart everything
   startTime = null;
